51a7cdb260af2a72eda5c42c97b7ffa4ea792979
regina-aurellana <imreginasalve@gmail.com>
2023-07-11 08:57:45 +0800

    Import Dupa

--- database/migrations/2023_03_20_070007_create_equipment_table.php
+++ database/migrations/2023_03_20_070007_create_equipment_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('equipment', function (Blueprint $table) {
             $table->id();
-            $table->string('item_code');
+            $table->string('item_code')->nullable();
             $table->string('name');
             $table->string('hourly_rate');
             $table->softDeletes();
--- database/migrations/2023_03_20_070106_create_materials_table.php
+++ database/migrations/2023_03_20_070106_create_materials_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('materials', function (Blueprint $table) {
             $table->id();
-            $table->string('item_code');
+            $table->string('item_code')->nullable();
             $table->string('name');
             $table->string('unit');
             $table->string('unit_cost');
